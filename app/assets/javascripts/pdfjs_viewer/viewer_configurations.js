f1cc8a4e284e3b354fb269d079a7138b7bc9ad87
Korol Oleksandr <korol.sas@gmail.com>
2022-02-14 11:00:11 +0200

    update pdf.js

--- app/assets/javascripts/pdfjs_viewer/viewer_configurations.js
+++ app/assets/javascripts/pdfjs_viewer/viewer_configurations.js
@@ -1,3 +1,1 @@
-var verbosity = document.querySelector('meta[name="pdfjs_viewer_verbosity"]').content;
-PDFJS.verbosity = PDFJS.VERBOSITY_LEVELS[verbosity];
-PDFJS.externalLinkTarget = PDFJS.LinkTarget.BLANK;
+PDFViewerApplication.preferences.set('externalLinkTarget', 2);
